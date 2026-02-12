5472f788a17afd0e144b0c37bd7bda2bedd1a491
rinudeepak <rinugeorgep@gmail.com>
2026-02-12 22:53:36 +0530

    deleted and modified database

--- database/add_task_module_tracking.sql
+++
@@ -1,4 +0,0 @@
--- Add module and lesson tracking to daily tasks
-ALTER TABLE `daily_tasks` 
-ADD COLUMN `module_number` INT NULL AFTER `day_number`,
-ADD COLUMN `lesson_number` INT NULL AFTER `module_number`;
